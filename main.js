d8513bcda22f1908c0e899292d498c697b6777f1
Alexandr <45771430+Fluiser@users.noreply.github.com>
2021-10-20 12:19:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -165,7 +165,7 @@ if(config['auto-images']) {
         const send = async(img) => {
             const w = img.rating === 'e' ? explicit : safe;
             if(w && img.score >= 1)
-                ['.png', '.jpg', '.jpeg', '.webm', '.bmp'].some(format => img.file_url.endsWith(format)) ?
+                ['.png', '.jpg', '.jpeg', '.webm', '.bmp', '.gif'].some(format => img.file_url.endsWith(format)) ?
                     await w.send(new MessageEmbed().setImage(img.file_url).setFooter(img.id + '/' + img.score)) :
                     await w.send(img.file_url);
         };
